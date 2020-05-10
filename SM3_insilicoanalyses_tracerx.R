694198e1ffb1ee97751db62cefb2c56ae80df794
Dinatale, Renzo G./Sloan Kettering Institute <dinatalr@mskcc.org>
2020-05-10 19:48:22 -0400

    final

--- SM3_insilicoanalyses_tracerx.R
+++ SM3_insilicoanalyses_tracerx.R
@@ -1,5 +1,3 @@
-#rm(list=ls())
-#setwd("/Users/dinatalr/Documents/MSKCC/rcc_translational/rcc_ith/analyses/scripts/final_code/")
 
 
 #############################################################################
